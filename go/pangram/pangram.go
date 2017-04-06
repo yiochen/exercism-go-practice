b136754963e1bd1bca826fb54d78bc89d9d6f6ee
yiochen <chen.yiou1992@gmail.com>
2017-04-05 23:47:24 -0400

    rm .idea

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,7 +1,30 @@
 package pangram
 
+import (
+	"github.com/golang-collections/go-datastructures/set"
+	"unicode"
+)
+
 const testVersion = 1
 
-func IsPangram(string) bool {
-	
+// IsPangram checks if a string is a pangram
+func IsPangram(input string) bool {
+	var hashSet = set.New()
+	var isGerman = false
+	for _, rune := range input {
+		if len(string(rune)) > 2 {
+			// not ascii
+			return false
+		}
+		if unicode.IsLetter(rune) {
+			hashSet.Add(unicode.ToUpper(rune), unicode.ToLower(rune))
+		}
+		if len(string(rune)) == 2 {
+			isGerman = true
+		}
+	}
+	if isGerman {
+		return hashSet.Len() == 59 // Almighty Google told me this number
+	}
+	return hashSet.Len() == 52
 }
