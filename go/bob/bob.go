0d4f875399530780df53b82143de7e08643471ad
yiochen <chen.yiou1992@gmail.com>
2017-04-06 21:57:17 -0400

    finished bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,3 +1,42 @@
 package bob // package name must match the package name in bob_test.go
 
+import (
+	"regexp"
+	"unicode"
+)
+
 const testVersion = 2 // same as targetTestVersion
+
+var isQuestion = regexp.MustCompile(`\?\s*$`)
+var isEmpty = regexp.MustCompile(`^[\v\s\x{00a0}\x{2002}]*$`)
+
+// Hey gives response as if bob was asked.
+func Hey(input string) string {
+	var containLowerCase = false
+	var containUpperCase = false
+	for _, rune := range input {
+		if unicode.IsLetter(rune) {
+			if unicode.ToLower(rune) == rune {
+				containLowerCase = true
+			}
+
+			if unicode.ToUpper(rune) == rune {
+				containUpperCase = true
+			}
+		}
+	}
+
+	if containUpperCase && !containLowerCase {
+		return "Whoa, chill out!"
+	}
+
+	if isQuestion.MatchString(input) {
+		return "Sure."
+	}
+
+	if isEmpty.MatchString(input) {
+		return "Fine. Be that way!"
+	}
+
+	return "Whatever."
+}
